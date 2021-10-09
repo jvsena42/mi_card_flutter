b9b0142dd4c08078229937da0f0a753b91782b44
jvsena42 <jvsena16@gmail.com>
2021-10-09 09:50:53 -0300

    feat: add container

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
             children: [
@@ -34,6 +34,52 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
+              Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+55 86 9 9800-6407',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'SourceSansPro-Regular',
+                            fontSize: 20.0),
+                      ),
+                    ],
+                  )),
+              Container(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+55 86 9 9800-6407',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'jvsena16@gmail.com',
+                            fontSize: 20.0),
+                      ),
+                    ],
+                  )),
             ],
           ),
         ),
